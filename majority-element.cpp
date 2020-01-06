ea539f65f23144d3089f94a10ae7ace823fbd3da
Kittiphong Xaysane <kxayasan@gmail.com>
2020-01-06 17:53:11 -0500

    Update majority-element.cpp

--- majority-element.cpp
+++ majority-element.cpp
@@ -1,12 +1,11 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int, int>myMap;
-        for(int i = 0; i < nums.size(); i++){
-            myMap[nums[i]]++;
-            if(myMap[nums[i]] > nums.size()/2){
-                return nums[i];
-            }
+int majorityElement(vector<int>& nums) {
+    map<int, int> countMap;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (++countMap[nums[i]] > nums.size()/2) {
+            return nums[i];
         }
     }
-};
+
+    return 0;
+}
