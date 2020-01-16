228dccaeb136b934ea3323e963019a8a24a36fb2
Kittiphong Xaysane <kxayasan@gmail.com>
2020-01-15 22:25:20 -0500

    Update valid-anagram.cpp

--- valid-anagram.cpp
+++ valid-anagram.cpp
@@ -1,21 +1,18 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        unordered_map<char, int> myMap;
-        for(int i = 0; i < s.length(); i++){
-            myMap[s[i]]++;
-        }
-        for(int i = 0; i < t.length(); i++){
-            myMap[t[i]]--;
-            if(myMap[t[i]] < 0){
-                return false;
-            }
-        }
-        for(int i = 0; i < s.length(); i++){
-            if(myMap[s[i]] != 0){
-                return false;    
-            }
-        }
-        return true;
+bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) {
+        return false;
     }
-};
+
+    map<char, int> charCount;
+    for (int i = 0; i < s.length(); i++) {
+        charCount[s[i]]++;
+        charCount[t[i]]--;
+    }
+    for (int i = 0; i < s.length(); i++) {
+        if (charCount[s[i]] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
